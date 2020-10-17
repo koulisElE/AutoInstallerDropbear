eb7127d68d2f093a461570fa7b5866412288d7d3
koulisElE <koulisElE@users.noreply.github.com>
2020-10-17 12:25:11 +0300

    Delete BadVpn.sh

--- BadVpn.sh
+++
@@ -1,9 +0,0 @@
-# install badvpn
-cd
-wget -O /usr/bin/badvpn-udpgw "https://github.com/shigeno143/Deb7AutoScriptVPS/raw/master/badvpn-udpgw"
-if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://github.com/shigeno143/Deb7AutoScriptVPS/raw/master/badvpn-udpgw64"
-fi
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
-chmod +x /usr/bin/badvpn-udpgw
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
