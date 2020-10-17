45f95a98c488c1ae1cf6351be8fba04f551f3480
koulisElE <koulisElE@users.noreply.github.com>
2020-10-17 12:03:21 +0300

    Update DropBearInstaller.sh

--- DropBearInstaller.sh
+++ DropBearInstaller.sh
@@ -28,14 +28,9 @@ apt-get update
 apt-get -y install nano iptables whois ngrep unzip unrar
 
 # install badvpn
-cd
-wget -O /usr/bin/badvpn-udpgw "https://github.com/shigeno143/Deb7AutoScriptVPS/raw/master/badvpn-udpgw"
-if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://github.com/shigeno143/Deb7AutoScriptVPS/raw/master/badvpn-udpgw64"
-fi
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
-chmod +x /usr/bin/badvpn-udpgw
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
+
+
 
 # setting port ssh
 cd
